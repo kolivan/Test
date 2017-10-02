cce12ea78a954cccb71087a7f2c15210045f2538
kolivan <ann.kolivanova@gmail.com>
2017-10-02 09:12:21 +0300

    ddt

--- Tests/TestDeliveryPrice.py
+++ Tests/TestDeliveryPrice.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 import unittest
 from Pages.CalculatePage import CalculatePage
+from ddt import ddt, data
 
 
+@ddt
 class Test(unittest.TestCase):
 
     def setUp(self):
@@ -13,13 +15,20 @@ class Test(unittest.TestCase):
         self.calcPage.click_country_dropdown(self.driver)
 
 
-    def test_check_auto_delivery_price_15(self):
+    @data((0, '0,00'),(15, '64,50'), (150, '645,00'), (151, '573,80'), (300, '240,00'),(301, '240,80'))# в этот тест добавляешь все значения, которые надо проверить
+    def test_check_auto_delivery_price_15_150(self,value):
+        a,b = value
         self.calcPage.input_country_name(self.driver, "Беларусь")
         self.calcPage.set_auto_delivery_mode(self.driver)
-        self.calcPage.input_weight(self.driver, '15')
+        self.calcPage.input_weight(self.driver, a)
+        self.driver.implicitly_wait(15)
         self.actual = self.calcPage.get_price(self.driver)
-        self.expected = 15*4.3
+        #self.actual1 = self.calcPage.get_cost(self.driver)
+        self.expected = b
         self.assertEqual(self.actual, self.expected)
+        #self.assertEqual(self.actual1, self.expected)
+
+
 
     def tearDown(self):
         self.driver.close()
